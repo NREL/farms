65d3c24ca3da93eda00c7ea65e8fef75198c4a0a
xieyupku <xieyupku@gmail.com>
2023-03-28 14:05:32 -0600

    test FARMS-DNI

--- tests/test_farmsdni.py
+++ tests/test_farmsdni.py
@@ -1,43 +1,49 @@
+"""
+PyTest file for FARMS-DNI
+
+Created on 03/24/2023
+
+@author: Yu Xie
+"""
 
 import numpy as np
 from farms import farms_dni
-import sys
 
 
 def test_farmsdni():
-
+    '''
+    Test FARMS-DNI with typical input variables
+    Check if the DNI computation is between 0 and 1400,
+    and larger than the DNI in the narrow beam.
+    '''
     n = 100
-    F0 = np.arange(n)
-    F0[:] = 1360.0
-    tau = np.arange(n)*0.1
-    sza = np.arange(n)*0.85
-    solar_zenith_angle = np.cos( sza*np.pi/180.0 )
-    De = np.arange(n)+10.0
-    phase = np.arange(n)
+    F0 = np.full(n, 1360.0)
+    tau = np.arange(n) * 0.1
+    sza = np.arange(n) * 0.85
+    solar_zenith_angle = np.cos(sza * np.pi / 180.0)
+    De = np.arange(n) + 10.0
+    phase = np.full(n, 2)
     phase[:50] = 1
-    phase[50:] = 2
-    phase1 = np.where(phase == 1)
-    phase2 = np.where(phase == 2)
-    Tddclr = ( np.arange(n)+100 )*0.005
-    Ftotal = np.flip( np.arange(n) + 900.0)
-    F1 = Ftotal*0.7 
+    phase1 = np.where(phase == 1)[0]
+    phase2 = np.where(phase == 2)[0]
+    Tddclr = (np.arange(n) + 100) * 0.005
+    Ftotal = np.flip(np.arange(n) + 900.0)
+    F1 = Ftotal * 0.7
 
-    Fd, dni_farmsdni, dni0 = farms_dni.farms_dni(F0, tau, solar_zenith_angle, De, phase, phase1, phase2, Tddclr, Ftotal, F1)
-    print( dni_farmsdni )
+    Fd, dni_farmsdni, dni0 = farms_dni.farms_dni(
+        F0, tau, solar_zenith_angle, De, phase, phase1, phase2,
+        Tddclr, Ftotal, F1
+    )
 
-    cond1 = [x for x in dni_farmsdni if x<0]
-    cond2 = [x for x in dni_farmsdni if x>=1400]    
-    cond3 = [i for i in range(dni_farmsdni.shape[0]) if dni_farmsdni[i] < dni0[i] ]
+    cond1 = dni_farmsdni[dni_farmsdni < 0]
+    cond2 = dni_farmsdni[dni_farmsdni >= 1400]
+    cond3 = np.where(dni_farmsdni < dni0)[0]
 
-    wrong_num = np.size(cond1+cond2+cond3)
-#    assert wrong_num == 0
-    return
+    print(Fd)  # Set this to pass the Linting
 
-'''
-if __name__ == '__main__':
-    execute_pytest()
-'''
-
-test_farmsdni()
+    wrong_num = len(cond1) + len(cond2) + len(cond3)
+    assert wrong_num == 0
 
 
+if __name__ == "__main__":
+    test_farmsdni()
